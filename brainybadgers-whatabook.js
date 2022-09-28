81646a7a9e49f9fc079be7b516ce1f124ea13ff3
Patrick Wolff <pat.wolff81@gmail.com>
2022-09-27 19:37:11 -0500

    whatabook initial commit

---
+++ brainybadgers-whatabook.js
@@ -0,0 +1,8 @@
+/* =========================================
+* Title: brainybadgers-whatabook.js
+* Authors: Patrick Wolff, Kailee Stephens, Anita Taylor
+* Date: 27 September 2022
+* Description: Queries for WhatABook project
+* ==========================================
+*/
+
