30655d08fed103b45224681e9bf1b80c942e368e
Anita L Taylor <98141576+aniltaylor@users.noreply.github.com>
2022-10-06 23:33:12 -0500

    Update brainybadgers-whatabook.js
    just added my name to the list of authors

--- brainybadgers-whatabook.js
+++ brainybadgers-whatabook.js
@@ -1,6 +1,6 @@
 /* =========================================
 * Title: brainybadgers-whatabook.js
-* Authors: Patrick Wolff, Kailee Stephens
+* Authors: Patrick Wolff, Kailee Stephens, Anita Taylor
 * Date: 27 September 2022
 * Description: Queries for WhatABook project
 * ==========================================
